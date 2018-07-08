77691c38f2925961f06a4a0daf08946d1c479007
Ben Cronin <benc@bens-mbp.lan>
2018-07-08 15:24:27 -0700

    Setup server to run client

--- server/main.js
+++ server/main.js
@@ -2,12 +2,12 @@ var express = require("express");
 var parser = require("body-parser");
 // var router = require("./routes.js");
 
-export default (app = express());
+app = express();
 
 app.set("port", 3000);
 app.use(parser.json());
 // app.use("/movies", router);
-app.use(express.static(`${__dirname}/../client`));
+app.use(express.static(__dirname + "/../client"));
 
 app.listen(app.get("port"));
 console.log("Listening on", app.get("port"));
